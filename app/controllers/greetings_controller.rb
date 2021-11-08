4320e5f38580f0cde4c785d141abe10b1454447d
themonster2015 <quynhyen.vothi@gmail.com>
2021-11-08 21:02:11 +0100

    generate greetings controller and seed database

---
+++ app/controllers/greetings_controller.rb
@@ -0,0 +1,8 @@
+class GreetingsController < ApplicationController
+    def index
+        @greeting = Greeting.all.sample
+        render json: {
+            greeting: @greeting
+    }.to_json
+    end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Greeting.create(text:'Hello')
+Greeting.create(text:'Xin chao')
+Greeting.create(text:'Hola')
+Greeting.create(text:'Nihao')
+Greeting.create(text:'Guten Tag')
+
+
+
+
